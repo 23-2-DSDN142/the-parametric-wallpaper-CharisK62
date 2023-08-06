4af5bb02f0c3ff92dba7cbf8be538eeac899e046
CharisK62 <Kangchar@myvuw.ac.nz>
2023-08-07 11:54:57 +1200

    updated my final settings.txt

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,76 +1,88 @@
 //your parameter variables go here!
 let rect_width  = 20;
 let rect_height = 20;
-var y = 100;
-var x = 17;
+var eyeY = 100; //100
+var eyeX = 17; //17
+var eyeSize = 10; //10
+var bodyY = 60; // 60
+var footY = 20; //20
+var beakY = 110; //110
+var fishingY = 40; //40
+var blushY = 112; // 112
+var blushShape = 5;// 5
+//let detailColour = color(0,51,104); // body colour
+If(blushShape > 10); {
+  fill (255,0,0) 
+  ellipse(80, blushY, 10, blushShape); // left blush
+  ellipse(120, blushY, 10, blushShape);
+}
+
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(GRID_WALLPAPER);
-  pWallpaper.resolution(FIT_TO_SCREEN);
+   pWallpaper.output_mode(GRID_WALLPAPER);
+  pWallpaper.resolution(A3);
   pWallpaper.show_guide(false); //set this to false when you're ready to print
 
-  //Grid settings
+  //Grid settings{
   pWallpaper.grid_settings.cell_width  = 350; //350
   pWallpaper.grid_settings.cell_height = 250; //350
   pWallpaper.grid_settings.row_offset  = 80;
-}
-
+} 
 function wallpaper_background() {
-  background(229, 204, 255); //light pink colour
-
+  background (229,204,255);  //(229,204,255)
 
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  
-  noStroke()
-  fill (255,178,102)
-  ellipse(70, 180, 10, 20); // left foot 1
-  ellipse(75,182,10,20) // left foot 2
-  ellipse(130,180,10,20) // right foot 1
-  ellipse(125,180,10,20) //right foot 2
+  let detailColour = color(0,51,104); // body colour //(0,51,104)
+//
 
-  // RGB values.
-let c = color(0, 51, 104); // navy blue colour
-fill(c);
+  noStroke(); 
+  fill (255,178,102) //255,178,102
+  ellipse(70, 180, 10, footY); // left foot 1
+  ellipse(75,182,10,footY) // left foot 2
+  ellipse(130,180,10,footY) // right foot 1
+  ellipse(125,180,10,footY) //right foot 2
+
+fill(detailColour);
 noStroke();
   ellipse(100, 100, 85, 80); //outside top
   ellipse(100, 140, 100, 90);//outside bottom
 
   triangle(35, 160, 60, 108, 60, 140); //left arm
-  triangle(140, 108, 120, 125, 163, 160);//right arm
+  triangle(140, 108, 120, 125, 163, 160);//right arm 
 
-fill(224,224,224);
-  ellipse(100, 100, 60, 60); //inside top
-  ellipse(100, 140, 70, 60); //inside bottom
- 
+  fill(244,244,244); // (244,244,244)
+  ellipse(100, 100, 60, bodyY); //inside top
+  ellipse(100, 140, 70, bodyY); //inside bottom
+
   fill (0,0,0); // black circle
-  ellipse(82, y, x, 18); // left eye
-  ellipse(118, y, x, 18); // right eye
+  ellipse(82, eyeY, eyeX, 18); // left eye
+  ellipse(118, eyeY, eyeX, 18); // right eye
 
   fill(255,255,255) //white circle
-  ellipse(85, 102, 8, 10); //left eye
-  ellipse(116, 102, 8, 10); //right eye
+  ellipse(85, 102, 8, eyeSize); //left eye
+  ellipse(116, 102, 8, eyeSize); //right eye
 
-  ellipse(80, 97, 4, 6); // left eye sparkle
-  ellipse(121, 97, 4, 6); // right eye sparkle
-
-  fill(225,153,153)
-  ellipse(80, 112, 10, 5); // left blush
-  ellipse(120, 112, 10, 5); // right blush
-
-  fill (255,178,102)
-  triangle(105, 110, 100, 117, 95, 110); //beak ellipse(70, 180, 10, 25);
+  ellipse(80, 97, 4,eyeSize - 4); // left eye sparkle
+  ellipse(121, 97, 4,eyeSize -4); // right eye sparkle
   
-  stroke(255, 255, 255);
+  fill(225,153,153)
+  ellipse(80, blushY, 10, blushShape); // left blush
+  ellipse(120, blushY, 10, blushShape); // right blush
+  
+  fill (255,178,102) //255,178,102
+  triangle(105, beakY, 100, 117, 95, beakY); //beak 
+  
+  stroke(255,255,255); 
 strokeWeight(3);
-  fill(127,202,239)
-  ellipse(-20, 160, 70, 50); // waterhole
+  fill(137, 207, 240) //137, 207, 240
+  ellipse(-20, 160, 70, 50); // waterhole 
 
-fill(253,176,209);
+fill(255,178,102); // (253,176,209)
   // Grayscale integer value.
 strokeWeight(2);
-stroke(201,143,168);
+stroke(201,143,168); //201,143,168
   beginShape();
   vertex(260, 100);
   quadraticVertex(240, 170, 311,180);
@@ -82,23 +94,22 @@ fill (0,0,0); // black circle
 
   strokeWeight(5);
 stroke(51);
-  line(20, 40, 45, 150); // fishing rod
+  line(20, fishingY, 45, 150); // fishing rod
 
-  strokeWeight(2);
+  strokeWeight(2); //2
 stroke(51);
-  line(4, 100, 20, 40); //fishing line
+  line(4, 100, 20, fishingY); //fishing line
 
   noFill();//
   ellipse(4,120,30,40) //fishing net ring
 
-  strokeWeight(1);
+  strokeWeight(1); //2
 stroke(51);
   noFill();//
   ellipse(12, 120, 30, 35); //fishingnet ring one
-  ellipse(20,120,20,27)
-  
+  ellipse(20,120,20,27);
 
-  strokeWeight(1);
+  strokeWeight(1); //1
   stroke(51);
     line(30, 120, 4, 100); // fishing net line 1
     line(30,120,-8,110); // fishiing net line2
